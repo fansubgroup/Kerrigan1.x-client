724f63942773011810056e1904fd73cffa6a8942
isinstance <super_big_hero@sina.com>
2016-09-21 10:05:18 -0400

    get new

--- Client.py
+++ Client.py
@@ -5,24 +5,25 @@ import sys
 import json
 
 class LoginServer:
-    
+
     def __init__(self, host_ip, port):
-        
+
         self.HOST_IP = host_ip
         self.PORT= port
         self.TIMEOUT = 20
-        
+
     def login(self):
-        
+
         client_socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         client_socks.settimeout(self.TIMEOUT)
-        
+
         client_socks.connect((self.HOST_IP, self.PORT))
-        
+
         HELLO = 'HELLO'
+
         client_socks.sendall(HELLO)
-        
+
         while True:
 
             data_json = client_socks.recv(4096)
@@ -48,7 +49,9 @@ class LoginServer:
 def main():
     
     init = LoginServer('127.0.0.1', 8001)
+
     init.login()
-    
+
 if __name__ == "__main__":
+
     main()
